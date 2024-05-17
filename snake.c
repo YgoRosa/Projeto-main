601e6882cfb0d14a7fbcc53f20fed3c7f7c79b3b
Matheus Cazé <mvco@cesar.school>
2024-05-17 10:33:36 -0300

    inicio de codigo

--- snake.c
+++ snake.c
@@ -1,4 +1,16 @@
-//teste teste galo
-//sport teste teste
-//hermes trismegisto
-//barão de mauá
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+
+//funcoes aqui
+
+int main(){
+    int cobra[300][2]; //matriz do corpinho da cobra
+    int maca[2]; //matriz da maçã
+    int pontos = 1; //pontuação do player
+    int cx = 2, cy = 2; //coordenadas x e y
+
+//return x;
+}
